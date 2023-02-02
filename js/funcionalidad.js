5a2e7ac7b259921038a514dc08988e198cf20100
Eugeniagassman <53584419+Eugeniagassman@users.noreply.github.com>
2023-02-02 11:26:11 -0300

    Add files via upload

---
+++ js/funcionalidad.js
@@ -0,0 +1,10 @@
+
+document.getElementById('ocultar').addEventListener('click', function(){
+    document.getElementById('cursos').style.display ='none';
+});
+
+document.getElementById('mostrar').addEventListener('click', function(){
+    document.getElementById('cursos').style.display ='block';
+});
+
+
